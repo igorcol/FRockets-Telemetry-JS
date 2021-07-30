deee07a2528c545d4dc2e9fdd1df96431b64d363
Igor Colombini <igor.colombini@gmail.com>
2021-07-29 22:53:32 -0300

    V 1.0 | Salvando os dados

--- main.js
+++ main.js
@@ -1,50 +1,85 @@
 const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+var fs = require('fs')
+const readline = require('readline')
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
 
+const PORT  = 3
+const BRATE = 9600
+var temp = []
+/*
+Cria csvWriter, header e arquivo
+*/
+const dir = "./amostras"
+CheckDir(dir)
+const dataIndex = (fs.readdirSync(dir).length + 1)
 
 const csvWriter = createCsvWriter({
-    path: "./amostras/file.csv",
+    path: `${dir}/amostra${dataIndex}.csv`,
     header: [
         {id: 'x', title: "X_VALUES"},
-        {id: 'y', title: "y_VALUES"},
+        {id: 'y', title: "Y_VALUES"},
     ]
-})
+});
+
+Main()
 
 
-// Main
-console.log("Software Iniciado")
-ReadSerial(3, 9600)
 
+
+// MAIN FUNCTION
+function Main() {
+    console.log("Software Iniciado")
+    ReadSerial(PORT, BRATE)
+}
 
 // Lê porta de comunicação
 function ReadSerial(portId, baudRate) {
     const port = new SerialPort(`COM${portId}`, { baudRate: baudRate })
-    
     const parser = new Readline()
     port.pipe(parser)
-    
-    const records = []
-    // LOOP 
-    parser.on("data", (data) => { // "6.44;1.87"
-        const splitDataArray = data.split(";") // [ '6.44', '1.87\r' ]
-        
-        const Xdata = splitDataArray[0]
-        const Ydata = splitDataArray[1]
+
+    // Main Loop
+    parser.on("data", (data) => { 
+        const splitDataArray = data.split(";") 
+        // data = "6.44;1.87"
+        // splitDataArray = [ '6.44', '1.87\r' ]
+        const Xdata = String(splitDataArray[0]) // Eixo X gráfico | TEMPO  
+        const Ydata = String(splitDataArray[1]) // Eixo Y gráfico | ALTURA 
 
         console.log(`${Xdata}  ||  ${Ydata.replace('\r', '')}`)
-
-        records.push(
-            {x: Xdata, y: Ydata},
-        )
         
+        SaveData([ 
+            {x: Xdata, y: Ydata} 
+        ])
 
-        console.log(records)
-        SaveData(records)
+        // PLOTAR GRAFICO
+
     });
 }
 
+// Exec before exit ( Ctrl + C )
+process.on('SIGINT', () => {
+    console.log("--SIGINT--")
+    process.exit()
+});
+process.on('exit', () => {
+    console.log("--EXIT--")
+    console.log(temp)
+});
+
+
 // Salva dados no CSV 
-function SaveData(data) {
-    csvWriter.writeRecords(data)
+async function SaveData(data) {
+    await csvWriter.writeRecords(data)
+}
+
+// Cria diretório se não existir
+function CheckDir(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+    }
+    return true
 }
